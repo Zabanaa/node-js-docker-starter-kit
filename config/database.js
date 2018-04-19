bc8cf88a1c0087df1cc3894e936960596e1888c9
Karim Cheurfi <karim.cheurfi@link-value.fr>
2018-04-19 15:43:45 +0200

    add try catch for database connection

--- config/database.js
+++ config/database.js
@@ -1,1 +1,15 @@
 // database config will go here
+const mongoose = require("mongoose")
+const { MONGO_DB_URI } = process.env
+mongoose.Promise = global.Promise
+
+try {
+    mongoose.connect(MONGO_DB_URI)
+} catch (error) {
+    /* handle error */
+    throw new Error("Something Happened", error)
+}
+
+const connection = mongoose.connection
+
+module.exports = connection
