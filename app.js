44d98dc57c2cc8dddc06aebbc26fdc742d943fc2
Karim Cheurfi <karim.cheurfi@link-value.fr>
2018-04-19 08:23:21 +0200

    add mongoose / connect app to DB

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
 const PORT = process.env.PORT || 5000
 
+mongoose.connect("mongodb://localhost:27017/mytestdb")
+
+const db = mongoose.connection
 
 app.get("/", (req, res) => {
     res.send("Hello New World")
 })
+
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => console.log("connection opened !"))
 
 app.listen(PORT, () => {
     console.log("Server listening on port: ", PORT)
