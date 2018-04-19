0b69c5b068ccbcff5c84c17f31c65ac7a73bc9af
Karim Cheurfi <karim.cheurfi@link-value.fr>
2018-04-19 09:05:41 +0200

    add post endpoint to create ninjas

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const Ninja = require("./schema")
 const PORT = process.env.PORT || 5000
 
 mongoose.connect("mongodb://localhost:27017/mytestdb")
+mongoose.Promise = global.Promise
 
 const db = mongoose.connection
+
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.send("Hello New World")
 })
 
+app.post("/ninjas", (req, res) => {
+    Ninja.create(req.body)
+        .then( ninja => res.send(ninja) )
+
+})
+
 db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", () => console.log("connection opened !"))
+
 
 app.listen(PORT, () => {
     console.log("Server listening on port: ", PORT)
